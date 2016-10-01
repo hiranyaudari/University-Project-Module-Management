7313e762737315f755cea0785bf5f2ac09324a35
HarshaPradeep <HarshDell@192.168.1.2>
2016-10-01 16:54:54 +0530

    Modified by Harsha Pradeep

--- resources/views/researchArea/addResearchArea.blade.php
+++ resources/views/researchArea/addResearchArea.blade.php
@@ -4,106 +4,107 @@
 
 
 @section('title')
-    <div class="row wrapper border-bottom white-bg page-heading">
-        <div class="col-lg-10">
-            <h2>Add Research Area</h2>
-            <ol class="breadcrumb">
+<div class="row wrapper border-bottom white-bg page-heading">
+    <div class="col-lg-10">
+        <h2>Add Research Area</h2>
+        <ol class="breadcrumb">
 
-            </ol>
-        </div>
-        <div class="col-lg-2">
-
-        </div>
+        </ol>
     </div>
+    <div class="col-lg-2">
+
+    </div>
+</div>
 
 @endsection
-
-
 {{--@section('subheader')--}}
-    {{--<h5>Add Research Area</h5>--}}
+{{--<h5>Add Research Area</h5>--}}
 {{--@endsection--}}
-
-
 
 @section('content')
 
 @if (Session::has('message_error'))
-        <div class="alert alert-danger" role="alert" id="divAlert" style="font-size: 14px">
-            {{ Session::get('message_error') }}
+<div class="alert alert-danger" role="alert" id="divAlert" style="font-size: 14px">
+    {{ Session::get('message_error') }}
+</div>
+@elseif(Session::has('message_success'))
+<div class="alert alert-success" role="alert" id="divAlert" style="font-size: 14px">
+    <span class="glyphicon glyphicon-envelope"></span> {{Session::get('message_success') }}
+</div>
+@endif
+<div class="col-lg-12">
+    <div class="ibox float-e-margins">
+        <div class="ibox-title">
+            <h5>All Research Area Details</h5>
+            <div class="ibox-tools">
+
+            </div>
         </div>
-    @elseif(Session::has('message_success'))
-        <div class="alert alert-success" role="alert" id="divAlert" style="font-size: 14px">
-            <span class="glyphicon glyphicon-envelope"></span> {{Session::get('message_success') }}
-        </div>
-    @endif
-        <div class="col-lg-12">
-            <div class="ibox float-e-margins">
-                <div class="ibox-title">
-                    <h5>All Research Area Details</h5>
-                    <div class="ibox-tools">
+        <div class="ibox-content">
 
-                    </div>
-                </div>
-                <div class="ibox-content">
+            <div class="table-responsive">
 
-                    <div class="table-responsive">
+                <table style="font-size: 13px" class="table table-striped table-bordered table-hover dataTables-example dataTable dtr-inline" id="DataTables_Table_0" role="grid" aria-describedby="DataTables_Table_0_info">
 
-                        <table style="font-size: 13px" class="table table-striped table-bordered table-hover dataTables-example dataTable dtr-inline" id="DataTables_Table_0" role="grid" aria-describedby="DataTables_Table_0_info">
-
-                                <thead>
-                                <tr role="row">
+                    <thead>
+                        <tr role="row">
 <!--                                    <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 100px;">Select</th>-->
-                                    <th class="sorting_asc" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Rendering engine: activate to sort column descending" style="width: 100px;" aria-sort="ascending">Research Area No</th>
-                                    <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 253px;">Research Area</th>
-                                    <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 162px;">Researcher I</th>
-                                    <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending" style="width: 115px;">Researcher II</th>
-                                    <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 550px;">Description</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                    
-                                @foreach($Allresearchareas as $researcharea)
-                                    <tr>
-<!--                                        <td ><input  type="checkbox" name="checkbox2[]" value="{{ $researcharea->id }}" ></td>-->
-                                        <td> {{ $researcharea->id }} </td>
-                                        <td> {{ $researcharea->research_area }} </td>
-                                        <td> {{ $researcharea->researcher_i }} </td>
-                                        <td> {{ $researcharea->researcher_ii }} </td>
-                                        <td> <?php echo wordwrap($researcharea->description,70,"\n",TRUE) ?></td>
-                                        <td>
-                                             {!! Form::open(['method' => 'DELETE', 'id' => 'deleteForm', 'action' => ['AddResearchArea@destroy', $researcharea->id ]]) !!}
-                                        <center>
-                                            {!! Form::button( '<i class="fa fa-trash fa-lg" title="Delete"></i>',
-                                                ['onclick' => 'deleteresearcharea()',
-                                                'class' => 'delete text-danger deleteForm',
-                                                'id' => 'btnDeleteProduct',
-                                                'data-id' => $researcharea->id ] ) !!}
-<!--                                            
-                                            <a onclick = "deletePresentationPanel()" id=""class="btn btn-sm btn-danger">Delete</a>-->
-                                            {!! Form::button( '<i class="fa fa-edit fa-lg" style="color: blue"title="Edit"></i>',
-                                                ['onclick' => 'editresearcharea()',
-                                                'class' => 'delete text-danger editeForm',
-                                                'id' => 'btnediteProduct',
-                                                'data-id' => $researcharea->id ] ) !!}    
-                                        </center>
-                                               {!! Form::close() !!}
-                                        </td>
-                                    </tr>
-                                @endforeach
+                            <th class="sorting_asc" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Rendering engine: activate to sort column descending" style="width: 100px;" aria-sort="ascending">Research Area No</th>
+                            <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 253px;">Research Area</th>
+                            <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 162px;">Researcher I</th>
+                            <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending" style="width: 115px;">Researcher II</th>
+                            <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 550px;">Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($Allresearchareas as $key=>$researcharea)
+                        <tr>
+<!--                        <td ><input  type="checkbox" name="checkbox2[]" value="{{ $researcharea->id }}" ></td>-->
+                            <td> {{ $researcharea->id }} </td>
+                            <td> {{ $researcharea->research_area }} </td>
+                            <td> {{ $researcharea->researcher_i }} </td>
+                            <td> {{ $researcharea->researcher_ii }} </td>
+                            <td> <?php echo $researcharea->research_area ;echo wordwrap($researcharea->description, 70, "\n", TRUE) ?></td>
+                            <td>
+                                {!! Form::open(['method' => 'DELETE', 'id' => 'deleteForm', 'action' => ['AddResearchArea@destroy', $researcharea->id ]]) !!}
+                    <center>
+                        <!--                        remove button-->
+                        {!! Form::button( '<i class="fa fa-trash fa-lg" title="Delete"></i>',
+                        ['onclick' => 'deleteresearcharea()',
+                        'class' => 'delete text-danger deleteForm',
+                        'id' => 'btnDeleteProduct',
+                        'data-id' => $researcharea->id ] ) !!}
+                        <!--                        end remove button-->
 
-                               </tbody>
-                                <tfoot>
-                                <tr>
-<!--                                    <th rowspan="1" colspan="1">Select</th>-->
-                                    <th rowspan="1" colspan="1">Research Area No</th>
-                                    <th rowspan="1" colspan="1">Research Area</th>
-                                    <th rowspan="1" colspan="1">Researcher I</th>
-                                    <th rowspan="1" colspan="1">Researcher II</th>
-                                    <th rowspan="1" colspan="1">Description</th>
-                                </tr>
-                                </tfoot>
-                            </table>
-                        {{--<div class="dataTables_info" id="DataTables_Table_0_info" role="status" aria-live="polite">Showing 21 to 30 of 57 entries</div>
+                        
+                        <!--                        update button-->
+                        <!--<a onclick = "deletePresentationPanel()" id=""class="btn btn-sm btn-danger">Delete</a>-->
+                        {!! Form::button( '<i class="fa fa-edit fa-lg" style="color: blue"title="Edit"></i>',
+                        ['onclick' => 'editresearcharea()',
+                        'class' => 'delete text-danger editeForm',
+                        'id' => 'btnediteProduct',
+                        'data-id' => $researcharea->id ] ) !!}
+                        <!--                        end update button-->
+                    </center>
+                    {!! Form::close() !!}
+                    </td>
+                    </tr>
+                    
+                    @endforeach
+
+                    </tbody>
+                    <tfoot>
+                        <tr>
+<!--                        <th rowspan="1" colspan="1">Select</th>-->
+                            <th rowspan="1" colspan="1">Research Area No</th>
+                            <th rowspan="1" colspan="1">Research Area</th>
+                            <th rowspan="1" colspan="1">Researcher I</th>
+                            <th rowspan="1" colspan="1">Researcher II</th>
+                            <th rowspan="1" colspan="1">Description</th>
+                        </tr>
+                    </tfoot>
+                </table>
+                {{--<div class="dataTables_info" id="DataTables_Table_0_info" role="status" aria-live="polite">Showing 21 to 30 of 57 entries</div>
 				<div class="dataTables_paginate paging_simple_numbers" id="DataTables_Table_0_paginate"><ul class="pagination">
 				<li class="paginate_button previous" aria-controls="DataTables_Table_0" tabindex="0" id="DataTables_Table_0_previous">
 				<a href="#">Previous</a></li><li class="paginate_button " aria-controls="DataTables_Table_0" tabindex="0">
@@ -114,23 +115,23 @@
 				<a href="#">5</a></li><li class="paginate_button " aria-controls="DataTables_Table_0" tabindex="0">
 				<a href="#">6</a></li><li class="paginate_button next" aria-controls="DataTables_Table_0" tabindex="0" id="DataTables_Table_0_next">
 				<a href="#">Next</a></li></ul></div></div>--}}
-                    </div>
-
-                </div>
             </div>
+
         </div>
+    </div>
+</div>
 
 <div class="row" style="margin-left: 0px">
 
     @if (count($errors) > 0)
 
-        <div class=" alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
+    <div class=" alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
     @endif
 
 
@@ -141,7 +142,7 @@
             <label>Research Area <strong style="color: red">*</strong></label>
             <input name="Research_Area" type="text" id="research" class="form-control"/>
         </div>
-        
+
         <div class="col-sm-10 form-group">
             <label>Researcher I</label>
             <input name="researcher1" type="text" id="person" class="form-control"/>
@@ -177,59 +178,59 @@
 @endsection
 
 
-
 @section('ValidationJavaScript')
-    <script src="//cdn.ckeditor.com/4.5.4/standard/ckeditor.js"></script>
-    <script>
-        $(document).ready(function(){
-            $('.summernote').ckeditor();
-        });
-    </script>
-    
-    <script>
-        
+<script src="//cdn.ckeditor.com/4.5.4/standard/ckeditor.js"></script>
+<script>
+                    $(document).ready(function () {
+                        $('.summernote').ckeditor();
+                    });
+</script>
+
+<script>
+
 //        $('.deleteForm').on('click', function(e) {  
 //
 //    var dataId = $(this).attr('data-id');
-//        alert(dataId);   
+       //alert($("tr", $(this).closest("DataTables_Table_0")).index(this));   
 //    
 //});
 
-function deleteresearcharea()
-{         
-    swal({
-        title: "Are you sure?",
-        text: "You will not be able to recover this Research Area!",
-        type: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#DD6B55",
-        confirmButtonText: "Yes!",
-        cancelButtonText: "Cancel!",
-        closeOnConfirm: false,
-        closeOnCancel: false },
-        function(isConfirm)
+//  deletes the selected research area
+    function deleteresearcharea()
+    {
+        swal({
+            title: "Are you sure?",
+            text: "You will not be able to recover this Research Area!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes!",
+            cancelButtonText: "Cancel!",
+            closeOnConfirm: false,
+            closeOnCancel: false},
+        function (isConfirm)
+        {
+            if (isConfirm)
             {
-                if (isConfirm)
-                {                 
-                    document.getElementById("deleteForm").submit();
-                    swal("Deleted!", "Research Area is deleted!", "success");
-                }
-                else
-                {     
-                    swal("Cancelled", "Your research area is safe :)", "error"); 
-                } 
-            });
+                document.getElementById("deleteForm").submit();
+                swal("Deleted!", "Research Area is deleted!", "success");
+            }
+            else
+            {
+                swal("Cancelled", "Your research area is safe :)", "error");
+            }
+        });
         return x;
         confirm("Do you want to delete this Research Area");
-}
-        
-        function editresearcharea() {
-            swal("KEEP CALM!", "it's \n COMING SOON!", "success");     
-            //swal("Updated!!", "Research Area is Updated", "success");      
-        }
-        
-        
+    }
+
+//    edit the selected research area
+    function editresearcharea() {
             
-   </script>
-    
+        swal("KEEP CALM!", "it's \n COMING SOON!", "success");
+        //swal("Updated!!", "Research Area is Updated", "success");      
+    }
+
+</script>
+
 @endsection
